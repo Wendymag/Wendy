5475c1eec98b16e34554ae75e232f58b82af21b2
Lusanda Magobiane <wendymagobiane001@gmail.com>
2021-06-08 10:26:51 +0200

    Wendy Magobiane

---
+++ script.js
@@ -0,0 +1,72 @@
+
+// // // //Array//
+// // var cars  = ["BMW", "VW", "TOYOTA", "BUGATTI"]
+// // // for(var x=cars.length-1; x>=0 ;x--){
+// // //     console.log(cars[x]);
+// // // }
+
+
+
+// // // cars.push("AUDI")
+
+
+
+// // cars.splice(1,0,"POLO")
+
+
+// // console.log(cars);
+
+// var person=["John", "Doe", 46];
+// // person [0]="John";
+// // person [1]="Doe";
+// // person[2]= 42;
+
+// var x =person.length;
+
+// for (let i = 0; i < x; i++) {
+//     const element = array[i]; 
+// }
+
+// console.log()
+
+
+// class Dogs{
+//     constructor(name,bark){
+//     this.dogsname= name;
+//     this.bark =  bark;
+//     };
+// }
+// let dog1 = new
+
+class House {
+
+    constructor(roomsize,color,window,doorheight){
+
+    
+        this.roomsize = roomsize;
+        this.color = color;
+        this.window = window;
+        this.doorheight = doorheight;
+
+       
+    };
+}
+
+
+let h1 = new House("500mm", "Blue" ,"2", "200m");
+let h2 = new House("600m", "Red", "3","210m")
+console.log(h1,+"\n"+h2)
+
+// r1.room = "bedroom";
+// r1.color ="blue";
+
+// r1.details
+// let r2 = new House();
+
+// r2.room = "bathorom";
+// r2.color = "red";
+
+// r2.details
+
+
+// console.log(r1);
